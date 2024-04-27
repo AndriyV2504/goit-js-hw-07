7b65e80991d0c3f5cf952c71d070060916837dd9
AndriyV2504 <154068131+AndriyV2504@users.noreply.github.com>
2024-04-27 18:42:04 +0300

    add-task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const colorSpan = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+function changeColor() {
+
+  const randomColor = getRandomHexColor();
+  
+  body.style.backgroundColor = randomColor;
+  
+  colorSpan.textContent = randomColor;
+}
+
+changeColorButton.addEventListener('click', changeColor);
