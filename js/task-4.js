49f0059b07265ba2c3f1bc8155eef60554841c99
AndriyV2504 <154068131+AndriyV2504@users.noreply.github.com>
2024-04-27 18:10:18 +0300

    add-task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,33 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector(".login-form");
+  
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+  
+      const formData = {};
+      const inputs = form.elements;
+  
+      for (let i = 0; i < inputs.length; i++) {
+        const input = inputs[i];
+  
+        if (input.type !== "submit") {
+          const key = input.name;
+          const value = input.value.trim();
+          formData[key] = value;
+        }
+      }
+  
+      const formValues = Object.values(formData);
+      const allFieldsFilled = formValues.every(value => value !== "");
+  
+      if (!allFieldsFilled) {
+        alert("All form fields must be filled in");
+        return;
+      }
+  
+      console.log(formData);
+  
+      form.reset();
+    });
+  });
+  
